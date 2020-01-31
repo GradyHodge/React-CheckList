f86f90c96723b148596fde888799d48cc4a6fdb9
Grady Hodge <gradywhodge@gmail.com>
2020-01-31 13:15:41 -0600

    initial commit completed ToDo React App

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList App from Traversy's React Crash Course</p>
+      <p>
+        <strong>YouTube Link: </strong>
+        <a href="https://www.youtube.com/watch?v=sBws8MSXN7A&t=467s">
+          Traversy's React JS Crash Course
+        </a>{" "}
+      </p>
+    </React.Fragment>
+  );
+}
